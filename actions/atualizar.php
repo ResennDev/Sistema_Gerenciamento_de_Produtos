37239365d6e0ab04d72ce91b47f28b949ce7655b
ResennDev <guilherme_dutra23@hotmail.com>
2026-06-20 11:59:44 -0300

    Refactor input handling in atualizar.php and salvar.php to ensure consistent formatting of product name and description

--- actions/atualizar.php
+++ actions/atualizar.php
@@ -5,6 +5,8 @@ include("../includes/conexao.php");
 $id = $_POST['id'];
 $nome = $_POST['nome'];
 $descricao = $_POST['descricao'];
+$nome = ucwords(strtolower(trim($nome)));
+$descricao = ucfirst(strtolower(trim($descricao)));
 $preco = str_replace('R$ ', '', $_POST['preco']);
 $preco = str_replace('.', '', $preco);
 $preco = str_replace(',', '.', $preco);
--- actions/salvar.php
+++ actions/salvar.php
@@ -4,6 +4,8 @@ include("../includes/conexao.php");
 
 $nome = $_POST['nome'];
 $descricao = $_POST['descricao'];
+$nome = ucwords(strtolower(trim($nome)));
+$descricao = ucfirst(strtolower(trim($descricao)));
 $preco = str_replace('R$ ', '', $_POST['preco']);
 $preco = str_replace('.', '', $preco);
 $preco = str_replace(',', '.', $preco);
